77aaae1de84def223a608abed11e63c2e232bf8d
97pongra <97pongra@BK-B3081.local.boden.se>
2015-12-01 09:27:45 +0100

    Initial commit
    Skeleton for the project

---
+++ flask_app.py
@@ -0,0 +1,16 @@
+"""Author: Pontus Granbom, 2015"""
+
+#importerar flask
+from flask import Flask, render_template
+
+#skapa flask-app
+app = Flask(__name__)
+
+@app.route('/')
+#@ = en decorator
+@app.route('/index/')
+def startpage():
+    return render_template("index.html")
+
+if (__name__ == "__main__"):
+    app.run()
